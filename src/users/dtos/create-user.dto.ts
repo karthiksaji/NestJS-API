f2a74dace85c767f3bc36d4e1521a336fc723223
sajeevkarthik87@gmail.com <kl05au@8938>
2024-12-18 12:19:42 +0530

    create-user-dto.ts file created

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,29 @@
+import {  IsEmail, IsNotEmpty, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class  CreateUserDto{
+@IsString()
+@IsNotEmpty()
+@MinLength(3)
+@MaxLength(9)
+firstName:string;
+
+@IsString()
+@IsOptional()
+@MaxLength(5)
+@MinLength(1)
+lastName:string;
+
+@IsString()
+@IsNotEmpty()
+@IsEmail()
+email:string;
+
+@IsString()
+@IsNotEmpty()
+@MaxLength(8)
+@Matches( /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,{
+    message:
+            'Password must be at least 8 characters long and contain both letters and numbers.'
+})
+password:string;
+}
