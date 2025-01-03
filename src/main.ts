5b85e963843a07f60834151e9621c8b84ab9cf6c
sajeevkarthik87@gmail.com <kl05au@8938>
2025-01-03 15:38:04 +0530

    installed swagger documention and updated in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder,SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
     forbidNonWhitelisted:true,
     transform:true
   }) );
+
+//swagger documentation
+
+const config=new DocumentBuilder().setVersion('1.0').build();
+
+// instantiate document
+
+const document=SwaggerModule.createDocument(app,config);
+SwaggerModule.setup('api',app,document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
