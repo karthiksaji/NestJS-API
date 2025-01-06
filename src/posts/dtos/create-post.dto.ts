c145dc8a96d6ca792d4b82b5095b5dfd2243e20e
sajeevkarthik87@gmail.com <kl05au@8938>
2025-01-06 15:59:02 +0530

    fol5 file11 completed

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,18 +1,27 @@
 
-import { IsArray, IsDate, IsEnum,IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength, ValidateNested } from "class-validator";
+import { IsArray, IsDate, IsEnum,IsISO8601,IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength, ValidateNested } from "class-validator";
 import { postStatus } from "../enums/postStatus.enum";
 import { postType } from "../enums/postType.enum";
 import { CreatePostMetaOptionsDto } from "./create-post-meta-options.dto";
 import { Type } from "class-transformer";
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
 
 export class CreatePostDto{
     @IsString()
     @MinLength(4)
     @IsNotEmpty()
+    @ApiProperty({
+        description:'this is the title for the blog spot',
+        example:'this is the title'
+    })
     title:string;
 
     @IsEnum(postType)
     @IsNotEmpty()
+    @ApiProperty({
+        enum:postType,
+        description:"possible values are 'post','page',story','series'"
+    })
     postType:postType;
 
     @IsString()
@@ -20,42 +29,85 @@ export class CreatePostDto{
     @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/,{
         message:'a slug should be small spaces'
     })
-
-    @IsString()
-    @IsNotEmpty()
+    @ApiProperty({
+        description:"for eg,my-url",
+        example:'my-blog-post'
+    })
     slug:string;
 
     @IsNotEmpty()
     @IsEnum(postStatus)
+    @ApiProperty({
+        enum:postStatus,
+        description:"possible values 'draft','scheduled','review','published'"
+    })
     status:postStatus;
 
     @IsString()
     @IsOptional()
+    @ApiPropertyOptional({
+        description:"this is the content of the post",
+        example:"the post content "
+    })
     content?:string;
 
     @IsJSON()
     @IsOptional()
+    @ApiPropertyOptional({
+        description:"serialize your json object else a validation error will be thrown"
+    })
     schema?:string;
 
     @IsOptional()
     @IsUrl()
+    @ApiPropertyOptional({
+        description:"featured image for your blog post",
+        example:'http://localhost.com/images/image1.jpg'
+    })
     featuredImageUrl?:string;
 
-    @IsDate()
+    @IsISO8601()
     @IsOptional()
+    @ApiPropertyOptional({
+        description:"the date on which  the blog is published",
+        example:'2024-03-16t07:46:32+000',
+    })
     PublishOn?:Date;
 
     @IsOptional()
     @IsArray()
     @IsString({each:true})
     @MinLength(3,{each:true})
+    @ApiPropertyOptional({
+        description:"array of tags passed as string values",
+        example:['nestjs','typescript'],
+    })
     tags?:string[];
 
     @IsOptional()
     @IsArray()
     @ValidateNested({each:true})
     @Type(()=>CreatePostMetaOptionsDto)
-    metaOptions:CreatePostMetaOptionsDto[];
+    @ApiPropertyOptional({
+        type:'array',
+        required:false,
+        items:{
+            type:'object',
+            properties:{
+                key:{
+                    type:'string',
+                    description:"the key can be any string identifier for your meta option",
+                    example:'sidebarenabled'
+                },
+                value:{
+                    type:'any',
+                    description:"any value can be any string identifier for your meta option",
+                    example:true
+                }
+            }
+        }
+    })
+    metaOptions?:CreatePostMetaOptionsDto[];
 
 
 }
