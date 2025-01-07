67881099f8f2e75280f0436489695f23548de858
sajeevkarthik87@gmail.com <kl05au@8938>
2025-01-07 10:46:22 +0530

    adding database coonection(postgresql)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,21 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule,
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      entities:[],
+      synchronize:true,
+      port:5432,
+      username:'postgres',
+      password:'12345',
+      host:'localhost',
+      database:'nestjs-blog'
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
